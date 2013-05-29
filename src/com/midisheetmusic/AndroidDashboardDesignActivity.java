358505876b92305c55a3272133b97cf235c92a88
John Chou <jychou@ucsd.edu>
2013-05-28 20:27:17 -0700

    fixed the entry class

--- src/com/midisheetmusic/AndroidDashboardDesignActivity.java
+++ src/com/midisheetmusic/AndroidDashboardDesignActivity.java
@@ -1,120 +1,80 @@
 package com.midisheetmusic;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
-
-import com.midisheetmusic.R;
+import android.widget.LinearLayout;
 
 public class AndroidDashboardDesignActivity extends Activity {
-
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Log.e("create", "NOF ACEBOOK");
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.dashboard_layout);
+/*		LinearLayout layout = (LinearLayout) findViewById(R.id.home_root);
+		Bitmap bmImg = decodeSampledBitmapFromResource(getResources(),
+				R.drawable.pianobckgd1, 768, 469);
+		BitmapDrawable background = new BitmapDrawable(bmImg);
+		int sdk = android.os.Build.VERSION.SDK_INT;
+		if (sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+			layout.setBackgroundDrawable(background);
+		} else {
+			layout.setBackground(background);
+		}*/
+		/*
+		 * setContentView(layout); // player.SetPiano(piano);
+		 * layout.requestLayout();
+		 */
+	}
 
-		/**
-		 * Creating all buttons instances
-		 * */
-		// Dashboard News feed button
-		Button btn_newsfeed = (Button) findViewById(R.id.btn_news_feed);
+	public static int calculateInSampleSize(BitmapFactory.Options options,
+			int reqWidth, int reqHeight) {
+		// Raw height and width of image
+		final int height = options.outHeight;
+		final int width = options.outWidth;
+		int inSampleSize = 1;
 
-		// Dashboard Friends button
-		Button btn_friends = (Button) findViewById(R.id.btn_friends);
+		if (height > reqHeight || width > reqWidth) {
 
-		// Dashboard Messages button
-		Button btn_messages = (Button) findViewById(R.id.btn_messages);
+			// Calculate ratios of height and width to requested height and
+			// width
+			final int heightRatio = Math.round((float) height
+					/ (float) reqHeight);
+			final int widthRatio = Math.round((float) width / (float) reqWidth);
 
-		// Dashboard Places button
-		Button btn_places = (Button) findViewById(R.id.btn_places);
+			// Choose the smallest ratio as inSampleSize value, this will
+			// guarantee
+			// a final image with both dimensions larger than or equal to the
+			// requested height and width.
+			inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+		}
 
-		// Dashboard Events button
-		Button btn_events = (Button) findViewById(R.id.btn_events);
+		return inSampleSize;
+	}
 
-		// Dashboard Photos button
-		Button btn_photos = (Button) findViewById(R.id.btn_photos);
+	public static Bitmap decodeSampledBitmapFromResource(Resources res,
+			int resId, int reqWidth, int reqHeight) {
 
-		/**
-		 * Handling all button click events
-		 * */
+		// First decode with inJustDecodeBounds=true to check dimensions
+		final BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inJustDecodeBounds = true;
+		BitmapFactory.decodeResource(res, resId, options);
 
-		// Listening to News Feed button click
-		btn_newsfeed.setOnClickListener(new View.OnClickListener() {
+		// Calculate inSampleSize
+		options.inSampleSize = calculateInSampleSize(options, reqWidth,
+				reqHeight);
 
-			@Override
-			public void onClick(View view) {
-				// Launching News Feed Screen
-				Intent i = new Intent(getApplicationContext(),
-						LevelActivity.class);
-				startActivity(i);
-			}
-		});
-
-		// Listening Friends button click
-		btn_friends.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View view) {
-				// Launching News Feed Screen
-				Intent i = new Intent(getApplicationContext(),
-						ChooseSongActivity.class);
-				startActivity(i);
-			}
-		});
-
-		// Listening Messages button click
-		btn_messages.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View view) {
-				// Launching News Feed Screen
-				Intent i = new Intent(getApplicationContext(),
-						SettingsActivity.class);
-				startActivity(i);
-			}
-		});
-
-		// Listening to Places button click
-		btn_places.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View view) {
-				// Launching News Feed Screen
-				Intent i = new Intent(getApplicationContext(),
-						TutorialMSActivity.class);
-				startActivity(i);
-			}
-		});
-
-		// Listening to Events button click
-		btn_events.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View view) {
-				// Launching News Feed Screen
-				// Intent i = new Intent(getApplicationContext(),
-				// EventsActivity.class);
-				// startActivity(i);
-			}
-		});
-
-		// Listening to Photos button click
-		btn_photos.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View view) {
-				// Launching News Feed Screen
-				// Intent i = new Intent(getApplicationContext(),
-				// PhotosActivity.class);
-				// startActivity(i);
-			}
-		});
+		// Decode bitmap with inSampleSize set
+		options.inJustDecodeBounds = false;
+		return BitmapFactory.decodeResource(res, resId, options);
 	}
 }
